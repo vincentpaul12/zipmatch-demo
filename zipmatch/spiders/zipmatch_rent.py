a2038879e81740d9d0afffedd6cd6606b3258e07
Vincent <vplcarpio@gmail.com>
2020-02-07 21:12:55 +0800

    Finished everything. Set item limit to 100

---
+++ zipmatch/spiders/zipmatch_rent.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import re
+import json
+
+from scrapy import Spider, Request
+from scrapy.linkextractors import LinkExtractor
+
+
+class ZipmatchRentSpider(Spider):
+    name = 'zipmatch_rent'
+    allowed_domains = ['zipmatch.com']
+    # NOTE: limit in the url query could be changed to display everything in one page
+    # For demo purposes, we used the default limit (45) to simulate pagination
+    # This scraper will get all condo for rent listings located at Metro Manila
+    listing_url = 'https://www.zipmatch.com/rent/condominium/metro-manila?p={}&limit=45&v=u&bedrooms=0&bathrooms=1'
+    
+    def start_requests(self):
+        page_number = 1
+        yield Request(self.listing_url.format(page_number),
+                      meta={'page_number': page_number})
+
+    def parse(self, response):
+        links = LinkExtractor(allow='/listings').extract_links(response)
+        for link in links:  # crawl each listing
+            yield response.follow(link, callback=self.handle_unit_response)
+        
+        page_number = response.meta.get('page_number')     
+        if links:  # if list is not empty, go to next page
+            yield Request(self.listing_url.format(page_number + 1),
+                          meta={'page_number': page_number + 1})
+
+    def handle_unit_response(self, response):
+        def parse_unit_details(response):
+            unit_details = {}
+            unit_details_div = response.xpath(
+                '//div[h3[text()="Unit Details"]]/following-sibling::div')
+
+            tables = unit_details_div.xpath('.//table')            
+            for table in tables:
+                keys = [x.strip() for x in table.xpath('.//th/text()[1]').getall()]
+                values = [x.strip() for x in table.xpath('.//td/text()[1]').getall()]
+                unit_details.update(dict(zip(keys, values)))
+
+            return unit_details
+        
+        def parse_geolocation(response):  # parse geolocation from <script>
+            zippy_script = response.xpath(
+                '//div[@id="main-container"]/script/text()').get()
+            geoloc_json = re.search(r'{.+(?=;)', zippy_script).group(0)
+            geoloc_json = json.loads(geoloc_json)['_geoloc']
+
+            return geoloc_json
+                
+        title = response.xpath('.//h1[@class="title"]/text()').get().strip()
+        image_links = response.xpath('.//div[@class="fotorama"]/img/@src').getall()
+        description = response.xpath('//p[@itemprop="description"]/text()').getall()
+        description = '|'.join([x.strip() for x in description])
+        
+        yield {
+            'title': title,
+            'image_links': image_links,
+            'description': description,
+            'link': response.url,
+            'unit_details': parse_unit_details(response),
+            'geolocation': parse_geolocation(response)
+        }
+
+
+# Goal: Get info about condos/apartments available for rent in Metro Manila via ZipMatch
+# Needed information: 
+# 1. Post title
+# 2. Description
+# 3. Unit details 
+# 4. Image links
